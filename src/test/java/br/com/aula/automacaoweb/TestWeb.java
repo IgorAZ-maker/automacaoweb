1118d2e08e077f21581bf5ebd11f688a3ee423dd
Igor Azevedo <igorazevedo2311@gmail.com>
2026-06-22 13:13:55 -0300

    aula6

--- src/test/java/br/com/aula/automacaoweb/TestWeb.java
+++ src/test/java/br/com/aula/automacaoweb/TestWeb.java
@@ -1,6 +1,9 @@
 package br.com.aula.automacaoweb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -9,22 +12,36 @@ import static org.junit.Assert.*;
 
 public class TestWeb {
 
-    @Test
-    public void primeiroTest(){
+    ChromeDriver driver;
+
+    @Before
+    public void inicializaTest(){
         WebDriverManager.chromedriver().setup();
-        ChromeDriver driver;
+
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://youtube.com");
+    }
+
+
+
+    @Test
+    public void primeiroTest(){
+
         String xpathTitulo = "//*[@id=\"sections\"]/ytd-guide-signin-promo-renderer/yt-formatted-string";
         WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
         String titulo = txtTitulo.getText();
         assertEquals("Faça login para curtir vídeos, comentar e se inscrever.", titulo);
 
-        driver.quit();
-
-
-
     }
+
+    @After
+    public void finalizaTest(){
+        driver.quit();
+    }
+
+
+
 }
+
