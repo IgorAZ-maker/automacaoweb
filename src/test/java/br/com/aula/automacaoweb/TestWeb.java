8a498006a623b4a2e7f74e0af9cf86cc74f494eb
Igor Azevedo <igorazevedo2311@gmail.com>
2026-07-02 16:10:19 -0300

    aula9

--- src/test/java/br/com/aula/automacaoweb/TestWeb.java
+++ src/test/java/br/com/aula/automacaoweb/TestWeb.java
@@ -1,7 +1,10 @@
 package br.com.aula.automacaoweb;
 
 import br.com.aula.core.Driver;
+import br.com.aula.pages.CursoPage;
+import br.com.aula.pages.PrincipalPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.jspecify.annotations.NonNull;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -10,34 +13,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 import static org.junit.Assert.*;
 
 public class TestWeb {
     WebDriver driver;
     Driver driverWeb;
+    PrincipalPage principalPage;
+    CursoPage cursoPage;
 
     @Before
     public void inicializaTest(){
         driverWeb = new Driver("chrome");
         driver = driverWeb.getDriver();
         driver.get("https://www.chronosacademy.com.br");
+        principalPage = new PrincipalPage(driver);
     }
 
     @Test
     public void primeiroTest(){
+        assertEquals("Porque Tempo É Conhecimento.", principalPage.getTitulo());
+    }
 
-        String xpathTitulo = "//section[2]//h4";
-        WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
-        String titulo = txtTitulo.getText();
-        assertEquals("Porque Tempo É Conhecimento.", titulo);
+    @Test
+    public void segundoTeste() throws InterruptedException {
+        cursoPage = new CursoPage(driver);
+        principalPage.clickBotao();
+        assertEquals("Conheça todos os nossos cursos.",cursoPage.getTitulo2());
+
     }
 
     @After
     public void finalizaTest(){
         driver.quit();
     }
-
-
-
 }
 
